776e076f9c9fa5b5bed56091bedf0e8c5c399dbc
VarnYard <142039672+VarnYard@users.noreply.github.com>
2024-02-13 21:43:55 -0700

    Varnier Lab 1

--- src/hdl/thirtyOneDayMonth.vhd
+++ src/hdl/thirtyOneDayMonth.vhd
@@ -56,23 +56,43 @@ library ieee;
 entity thirtyOneDayMonth is 
   port(
 	i_A : in std_logic; -- one of four inputs
-	
-	
-	
-						-- output
+	i_B : in std_logic;
+	i_C : in std_logic; 
+	i_D : in std_logic; 
+	o_Y : out std_logic	-- output
   );
 end thirtyOneDayMonth;
 
 architecture thirtyOneDayMonth_arch of thirtyOneDayMonth is 
 	-- include components declarations and signals
 	
+signal w_sel : std_logic_vector (2 downto 0); -- MUX sel
+
 	--signals internal to the architecture are declared and initialized such as w_sel
   
 begin
 	-- CONCURRENT STATEMENTS---------------------------------------
 	--assigning names to reflect original schematics (for ease of understanding if you wish to)
 	w_sel(0) <= i_C;	-- one
-	--finish assigning signals
+	w_sel(2) <= i_A;
+	w_sel(1) <= i_B;
+	
+	process(w_sel, i_D)
+	
+	begin
+	   case w_sel is
+	       when "000" => o_Y <= i_D;
+	       when "001" => o_Y <= i_D;
+	       when "010" => o_Y <= i_D;
+	       when "011" => o_Y <= i_D;
+	       when "100" => o_Y <= not i_D;
+           when "101" => o_Y <= not i_D;
+           when "110" => o_Y <= not i_D;
+           when "111" => o_Y <= not i_D;
+           when others => o_Y <= i_D;
+       end case; 
+
+	end process; 	
 	
 	--enter your logic here to implement the mux.  See VHDL reference sheet for MUX syntax.	
 	---------------------------------------------------------------	
